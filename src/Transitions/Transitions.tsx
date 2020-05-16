6efc4f8308cc1cfa3e64c91765e2d56900b60c58
Rizaldidev <rizaldi@stockbit.com>
2020-05-16 16:38:13 +0700

    transition

--- src/Transitions/Transitions.tsx
+++ src/Transitions/Transitions.tsx
@@ -3,12 +3,13 @@ import {
   Dimensions,
   ImageStyle,
   StyleSheet,
-  View,
   ViewStyle
 } from "react-native";
+import Animated, {Transitioning, Transition, TransitioningView} from 'react-native-reanimated';
 
-import { FlexibleCard as Card, StyleGuide, cards } from "../components";
+import { FlexibleCard as Card, StyleGuide, cards, Selection } from "../components";
 
+const {} = Animated;
 interface Layout {
   id: string;
   name: string;
@@ -24,14 +25,77 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: StyleGuide.palette.background
   }
-});
+}); 
+
+const coloumn : Layout  = {
+  id: "column",
+  name: "Column",
+  layout: {
+    container: {
+      flexDirection: "column"
+    }
+  }
+}
+
+const row: Layout = {
+  id: "row",
+  name: "Row",
+  layout: {
+    container: {
+      flexDirection: 'row',
+      alignItems: 'center'
+    }
+  }
+}
+
+const wrap: Layout = {
+  id: "wrap",
+  name: "Wrap",
+  layout: {
+    container: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      flexWrap: 'wrap'
+    },
+    child: {
+      flex: 0,
+      width: width / 2 - StyleGuide.spacing * 2
+    }
+  }
+}
+
+const layouts = [coloumn, row, wrap]
+const transition = <Transition.Change durationMs={400} interpolation="easeInOut" />
 
 export default () => {
+  const ref = React.useRef<TransitioningView>(null);
+  const [currentLayout, setCurrenLayout] = React.useState(layouts[0].layout)
   return (
-    <View style={styles.container}>
+    <>
+    <Transitioning.View 
+      style={[styles.container, currentLayout.container]}
+      {...{ref, transition}}
+    >
       {cards.map(card => (
-        <Card key={card.id} style={{}} {...{ card }} />
+        <Card key={card.id} style={currentLayout.child} {...{ card }} />
       ))}
-    </View>
+      
+    </Transitioning.View>
+    {
+        layouts.map(layout => (
+          <Selection 
+            key={layout.id} 
+            name={layout.name} 
+            isSelected={layout.layout === currentLayout} 
+            onPress={() => {
+              if(ref.current) {
+                ref.current.animateNextTransition()
+              }
+              setCurrenLayout(layout.layout)
+            }} 
+          />
+        ))
+      }
+    </>
   );
 };
