a18fdf48ccfd9a99d2ab3470384076536f2f9e02
Rizaldidev <rizaldi@stockbit.com>
2020-05-16 18:49:38 +0700

    useTransition()

--- src/UseTransition/useTransition.tsx
+++ src/UseTransition/useTransition.tsx
@@ -3,7 +3,7 @@ import { Dimensions, StyleSheet, View } from "react-native";
 import Animated, { Easing } from "react-native-reanimated";
 import {
   bInterpolate,
-  transformOrigin,
+  // transformOrigin,
   useTransition
 } from "react-native-redash";
 
@@ -15,20 +15,51 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "flex-end"
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
 
+const transformOrigin = -1 * (width / 2 - StyleGuide.spacing * 2)
+
 export default () => {
+  const [toggled, setToggled] = React.useState<0 | 1>(0);
+  const transition = useTransition(toggled);
+
   return (
     <View style={styles.container}>
       {cards.map((card, index) => {
+        
+        // let direction = 0;
+        // if(index === 0) {
+        //   direction = -1
+        // } else if(index === 2) {
+        //   direction = 1
+        // }
+        const direction = interpolate(index, {
+          inputRange: [0, 1, 2],
+          outputRange: [-1, 0, 1]
+        })
+        const rotate = multiply(direction, interpolate(transition, {
+          inputRange: [0, 1],
+          outputRange: [0, Math.PI / 6]
+        }));
         return (
-          <Animated.View key={card.id}>
+          <Animated.View key={card.id} style={[styles.overlay, {
+            transform: [
+              { translateX: transformOrigin },
+              { rotate },
+              { translateX: -transformOrigin }
+            ]
+          }]}>
             <Card {...{ card }} />
           </Animated.View>
         );
       })}
-      <Button label="Start" primary onPress={() => true} />
+      <Button label={toggled ? "Reset" : "Start" } primary onPress={() => setToggled(toggled ^ 1)} />
     </View>
   );
 };
